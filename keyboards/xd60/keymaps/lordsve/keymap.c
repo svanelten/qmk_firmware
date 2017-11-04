448365fab05073505b95a32cfc8cfbcd8d2825d8
svanelten <sve@gravitydesign.de>
2017-11-04 20:51:43 +0100

    shifted bottom row mo-functionality to the right for easier usage

--- keyboards/xd60/keymaps/lordsve/keymap.c
+++ keyboards/xd60/keymaps/lordsve/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,                KC_NO,    \
     MO(_FL),    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_NUHS,                KC_ENT,   \
     KC_LSFT,    DE_LESS,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO,      KC_RSFT,    KC_NO,    \
-    KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,                                                     KC_RALT,    MO(_FL),    KC_NO,      MO(_NL),    KC_RCTL),
+    KC_LCTL,    KC_LGUI,    KC_LALT,                            KC_SPC,                                                     KC_RALT,    KC_MENU,    KC_NO,      MO(_FL),    MO(_NL)),
 
   // 1: Function Layer
   [_FL] = KEYMAP(
     KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_TRNS,    KC_NO,    \
     KC_TRNS,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_NO,    \
     KC_TRNS,    KC_MPLY,    KC_MSTP,    KC_MPRV,    KC_MNXT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_HOME,    KC_UP,      KC_PGUP,    KC_PGDN,    KC_TRNS,                KC_ENT,   \
-    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_END,     KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_TRNS,    KC_TRNS,    KC_NO,    \
-    KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,                                                    KC_DEL,     KC_TRNS,     KC_NO,      KC_TRNS,    KC_INS),
+    KC_TRNS,    KC_INS,     KC_DEL,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_END,     KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_TRNS,    KC_TRNS,    KC_NO,    \
+    KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,                                                    KC_DEL,     KC_INS,     KC_NO,      KC_TRNS,    KC_TRNS),
 
   // 1: Number Layer
   [_NL] = KEYMAP(
