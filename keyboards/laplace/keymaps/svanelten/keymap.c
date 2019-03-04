7a87875a3743d14015c0fb85811d96edb5c6854e
svanelten <sve@joinmedia.de>
2019-03-04 09:28:33 +0100

    Added macros to prog layer

--- keyboards/laplace/keymaps/svanelten/keymap.c
+++ keyboards/laplace/keymaps/svanelten/keymap.c
@@ -10,13 +10,25 @@
 #define _______ KC_TRNS
 #define ___x___ KC_NO
 
+enum custom_keycodes
+{
+  MC_GREETS = SAFE_RANGE,
+  MC_GSTAT, // git status
+  MC_GBRA,  // git branch
+  MC_GADD,  // git add .
+  MC_GCOM,  // git commit -m ""
+  MC_GREM,  // git reset
+  MC_GRST,  // git reset --hard
+  MC_GPUL,  // git pull
+  MC_GPSH,  // git push
+  MC_GLOG   // git log
+};
+
 #define FN1 MO(_FN1)
 #define FN2 MO(_FN2)
 #define PROG MO(_PROG)
 #define SP_FN1 LT(_FN1, KC_SPACE)
 #define SP_FN2 LT(_FN2, KC_SPACE)
-// #define BS_FN1 LT(_FN1, KC_BSPC)
-// #define BS_FN2 LT(_FN2, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -28,23 +40,106 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN1] = LAYOUT(
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,   _______,
+    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     DE_SS,    DE_SS,    _______,
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    _______,  _______,  _______,  DE_HASH,  _______,
     _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   ),
 
   [_FN2] = LAYOUT(
-    KC_TILD,  DE_AT,    _______,  DE_EURO,  _______,  _______,  DE_SLSH,  DE_UE,    DE_BSLS,  DE_OE,    KC_INS,   KC_DEL,   KC_DEL,
-    _______,  DE_AE,    DE_SS,    _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_UP,    KC_PGDN,  _______, 
+    KC_GRV,   DE_AT,    _______,  DE_EURO,  _______,  _______,  DE_SLSH,  DE_UE,    DE_BSLS,  DE_OE,    KC_INS,   KC_DEL,   KC_DEL,
+    _______,  DE_AE,    DE_SS,    _______,  _______,  MC_GREETS,_______,  _______,  KC_PGUP,  KC_UP,    KC_PGDN,  _______, 
     _______,  DE_LESS,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_END,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   ),
 
   [_PROG] = LAYOUT(
-    DE_RING,  _______,  _______,  _______,  _______,  _______,  DE_LESS,  DE_MORE,  DE_LBRC,  DE_RBRC,  DE_QST,   _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  DE_LCBR,  DE_RCBR,  DE_LPRN,  DE_RPRN,  _______,
-    _______,  DE_PIPE,  _______,  _______,  _______,  _______,  _______,  DE_DQOT,  DE_QUOT,  DE_GRV,   DE_TILD,  _______,
+    DE_RING,  _______,  MC_GPUL,  MC_GPSH,  MC_GLOG,  _______,  DE_LESS,  DE_MORE,  DE_LBRC,  DE_RBRC,  DE_QST,   _______,  _______,
+    _______,  _______,  MC_GSTAT, MC_GADD,  MC_GREM,  MC_GRST,  _______,  DE_LCBR,  DE_RCBR,  DE_LPRN,  DE_RPRN,  _______,
+    _______,  DE_PIPE,  MC_GBRA,  _______,  _______,  _______,  _______,  DE_DQOT,  DE_QUOT,  DE_GRV,   DE_TILD,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
   )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+  switch (keycode)
+  {
+    case MC_GREETS:
+      if (record->event.pressed)
+      {
+        // when keycode MC_GREETS is pressed send string and do nothing else
+        SEND_STRING("Viele Gr" SS_TAP(X_LBRACKET) SS_TAP(X_MINUS) "e," SS_TAP(X_ENTER) "sve");
+        return true;
+      }
+      else
+      {
+        // nothing on release
+      }
+      break;
+    case MC_GSTAT:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git status" SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    case MC_GBRA:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git branch" SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    case MC_GADD:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git add ." SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    case MC_GREM:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git reset" SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    case MC_GCOM:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git commit " SS_TAP(X_SLASH) "m " SS_LSFT("2") SS_LSFT("2") SS_TAP(X_LEFT));
+        return true;
+      }
+      break;
+    case MC_GRST:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git reset " SS_TAP(X_SLASH) SS_TAP(X_SLASH) "hard");
+        return true;
+      }
+      break;
+    case MC_GPUL:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git pull" SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    case MC_GPSH:
+      if (record->event.pressed)
+      {
+          SEND_STRING("git push");
+          return true;
+      }
+      break;
+    case MC_GLOG:
+      if (record->event.pressed)
+      {
+        SEND_STRING("git log" SS_TAP(X_ENTER));
+        return true;
+      }
+      break;
+    }
+    return true;
+}
