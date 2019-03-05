fb8e9a308d40de0e928556acba20d79cfddc2d0f
svanelten <sve@joinmedia.de>
2019-03-06 00:22:21 +0100

    Updated macros

--- keyboards/laplace/keymaps/svanelten/keymap.c
+++ keyboards/laplace/keymaps/svanelten/keymap.c
@@ -1,49 +1,54 @@
 #include QMK_KEYBOARD_H
+
 // include german keymap for umlauts and special chars
 #include "keymap_extras/keymap_german.h"
 
+// layer definitions
 #define _BASE 0
 #define _FN1 1
 #define _FN2 2
 #define _PROG 3
 
+// keycode definitions
 #define _______ KC_TRNS
 #define ___x___ KC_NO
 
 enum custom_keycodes
 {
-  MC_GREETS = SAFE_RANGE,
-  MC_GSTAT, // git status
-  MC_GBRA,  // git branch
-  MC_GADD,  // git add .
-  MC_GCOM,  // git commit -m ""
-  MC_GREM,  // git reset
-  MC_GRST,  // git reset --hard
-  MC_GPUL,  // git pull
-  MC_GPSH,  // git push
-  MC_GLOG   // git log
+  MC_GREETS = SAFE_RANGE, // email greeting
+  MC_GSTAT,               // git status
+  MC_GBRA,                // git branch
+  MC_GADD,                // git add .
+  MC_GCOM,                // git commit -m ""
+  MC_GREM,                // git reset
+  MC_GRST,                // git reset --hard
+  MC_GPUL,                // git pull
+  MC_GPSH,                // git push
+  MC_GLOG                 // git log
 };
 
+// layer switch / taps
 #define FN1 MO(_FN1)
 #define FN2 MO(_FN2)
 #define PROG MO(_PROG)
 #define SP_FN1 LT(_FN1, KC_SPACE)
 #define SP_FN2 LT(_FN2, KC_SPACE)
 
+// keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     DE_Z,     KC_U,     KC_I,     KC_O,     KC_P,     KC_P,   KC_BSPC,
     KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     DE_PLUS,  DE_HASH,
     KC_LSFT,  DE_Y,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   DE_MINS,  KC_ENT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,  PROG,     SP_FN1,   SP_FN2,   PROG,     KC_ALGR,  KC_RGUI,  KC_RCTL
+    KC_LCTL,  KC_LGUI,  KC_LALT,  PROG,     SP_FN1,             SP_FN2,             PROG,     KC_ALGR,  KC_RGUI,  KC_RCTL
   ),
 
   [_FN1] = LAYOUT(
     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     DE_SS,    DE_SS,    _______,
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    _______,  _______,  _______,  DE_HASH,  _______,
     _______,  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+    _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
   ),
 
   [_FN2] = LAYOUT(
@@ -54,13 +59,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_PROG] = LAYOUT(
-    DE_RING,  _______,  MC_GPUL,  MC_GPSH,  MC_GLOG,  _______,  DE_LESS,  DE_MORE,  DE_LBRC,  DE_RBRC,  DE_QST,   _______,  _______,
-    _______,  _______,  MC_GSTAT, MC_GADD,  MC_GREM,  MC_GRST,  _______,  DE_LCBR,  DE_RCBR,  DE_LPRN,  DE_RPRN,  _______,
-    _______,  DE_PIPE,  MC_GBRA,  _______,  _______,  _______,  _______,  DE_DQOT,  DE_QUOT,  DE_GRV,   DE_TILD,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+    DE_RING,  MC_GLOG,  MC_GPUL,  MC_GPSH,  MC_GREM,  _______,  DE_LESS,  DE_MORE,  DE_LBRC,  DE_RBRC,  DE_QST,   _______,  _______,
+    _______,  MC_GADD,  MC_GSTAT, _______,  _______,  _______,  _______,  DE_LCBR,  DE_RCBR,  DE_LPRN,  DE_RPRN,  _______,
+    _______,  DE_PIPE,  MC_GRST,  MC_GCOM,  _______,  MC_GBRA,  _______,  DE_DQOT,  DE_QUOT,  DE_GRV,   DE_TILD,  _______,
+    _______,  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______
   )
 };
 
+// macro triggers
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
   switch (keycode)
@@ -69,7 +75,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
       if (record->event.pressed)
       {
         // when keycode MC_GREETS is pressed send string and do nothing else
-        SEND_STRING("Viele Gr" SS_TAP(X_LBRACKET) SS_TAP(X_MINUS) "e," SS_TAP(X_ENTER) "sve");
+        SEND_STRING("Gr" SS_TAP(X_LBRACKET) SS_TAP(X_MINUS) "e," SS_TAP(X_ENTER) "sve");
         return true;
       }
       else
@@ -94,14 +100,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     case MC_GADD:
       if (record->event.pressed)
       {
-        SEND_STRING("git add ." SS_TAP(X_ENTER));
+        SEND_STRING("git add .");
         return true;
       }
       break;
     case MC_GREM:
       if (record->event.pressed)
       {
-        SEND_STRING("git reset" SS_TAP(X_ENTER));
+        SEND_STRING("git reset");
         return true;
       }
       break;
