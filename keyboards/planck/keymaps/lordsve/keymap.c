dfc7a82f71985ca0447b49081132abdf4145d095
svanelten <sve@joinmedia.de>
2019-04-26 10:02:48 +0200

    Trigger ALT Layer on lower btn, move lower right

--- keyboards/planck/keymaps/lordsve/keymap.c
+++ keyboards/planck/keymaps/lordsve/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    DE_Z,      KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,    KC_H,      KC_J,     KC_K,     KC_L,     DE_PLUS,  DE_HASH,
     KC_LSFT,  DE_Y,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,      KC_M,     KC_COMM,  KC_DOT,   DE_MINS,  KC_ENT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,  MOUSE,    RAISE,    ALT,     ALT,       LOWER,    ___x___,  KC_ALGR,  KC_RGUI,  KC_RCTRL
+    KC_LCTL,  KC_LGUI,  KC_LALT,  MOUSE,    RAISE,    ALT,     ALT,       ALT,      LOWER,    KC_ALGR,  KC_RGUI,  KC_RCTRL
   ),
 
   /* Raise
